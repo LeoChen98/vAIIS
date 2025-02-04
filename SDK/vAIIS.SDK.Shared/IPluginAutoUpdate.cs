8be8ad31cba4c0fa0ca1e5ef00e8bd707c1ab1b2
Leo Chen <Leo@zhangbudademao.com>
2025-02-04 18:45:28 +0800

    adjust project struct and ignore ridder profiles

--- SDK/vAIIS.SDK.Shared/IPluginAutoUpdate.cs
+++ SDK/vAIIS.SDK.Shared/IPluginAutoUpdate.cs
@@ -26,14 +26,16 @@ public interface IPluginAutoUpdate
     /// <param name="oldVersion">Old version of the plugin. Can be <see langword="null"/>.</param>
     /// <remarks>
     /// note:
-    /// <para>- This method will be called ( in NEW version dll ) after the plugin is updated.</para>
-    /// <para>- Can be used to update the plugin settings or do any other required tasks.</para>
-    /// <para>- If nothing to do, <see langword="return"/> directly.</para>
-    /// <para>
-    /// - The host will try to fill the <paramref name="oldVersion"/>, but if no data, fill with
+    /// <list type="bullet">
+    /// <item>This method will be called ( in NEW version dll ) after the plugin is updated.</item>
+    /// <item>Can be used to update the plugin settings or do any other required tasks.</item>
+    /// <item>If nothing to do, <see langword="return"/> directly.</item>
+    /// <item>
+    /// The host will try to fill the <paramref name="oldVersion"/>, but if no data, fill with
     ///    <see langword="null"/>. <see cref="NullReferenceException"/> should be expected if this
     ///    <paramref name="oldVersion"/> is used in the method.
-    /// </para>
+    /// </item>
+    /// </list>
     /// </remarks>
     /// <returns></returns>
     public Task FinishUpdateAsync(string? oldVersion);
