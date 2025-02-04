49cbde1c8a022b5e8a78e4a49943fd6d95948754
Leo Chen <Leo@zhangbudademao.com>
2025-02-04 18:58:52 +0800

    set Program class as static

--- vAIIS.wpf/Program.cs
+++ vAIIS.wpf/Program.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace vAIIS.Wpf
 {
-    internal class Program
+    internal static class Program
     {
         [STAThread]
         private static void Main(string[] args)
