1b66854eca75ed9435f5f24669305f217ab43133
Leo Chen <Leo@zhangbudademao.com>
2025-02-04 19:30:47 +0800

    fix `LogEventLevel` missing

--- vAIIS.wpf/Program.cs
+++ vAIIS.wpf/Program.cs
@@ -2,11 +2,7 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Serilog.Events;
 
 namespace vAIIS.Wpf
 {
